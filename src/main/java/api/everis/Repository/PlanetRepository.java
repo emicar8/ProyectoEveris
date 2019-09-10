21c16312964b707c6bef3e7882c248ec8ff8eae5
Emile <emicaron95@gmail.com>
2019-09-10 16:44:31 -0300

    Implementacio de las clases en repository

--- src/main/java/api/everis/Repository/PlanetRepository.java
+++ src/main/java/api/everis/Repository/PlanetRepository.java
@@ -1,5 +1,11 @@
 package api.everis.Repository;
 
-public interface PlanetRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import api.everis.Entity.Planet;
+
+@Repository
+public interface PlanetRepository extends JpaRepository<Planet, Integer> {
 
 }
--- src/main/java/api/everis/Repository/StarRepository.java
+++ src/main/java/api/everis/Repository/StarRepository.java
@@ -1,5 +1,11 @@
 package api.everis.Repository;
 
-public interface StarRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import api.everis.Entity.Star;
+
+@Repository
+public interface StarRepository extends JpaRepository<Star, Integer>{
 
 }
