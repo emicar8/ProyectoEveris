386999aed6ab999b65f4c3002394eba5e6f01ee1
Emile <emicaron95@gmail.com>
2019-09-10 19:28:21 -0300

    Implementacion de capa de servicios

--- src/main/java/api/everis/Service/PlanetService.java
+++ src/main/java/api/everis/Service/PlanetService.java
@@ -1,5 +1,135 @@
 package api.everis.Service;
 
-public class PlanetService {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
+import javax.transaction.Transactional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import api.everis.DTO.PlanetDTO;
+import api.everis.Entity.Planet;
+import api.everis.Repository.PlanetRepository;
+
+@Service
+public class PlanetService {
+	
+	private PlanetRepository planetRepository;
+
+	public PlanetService(PlanetRepository planetRepository) {
+		this.planetRepository = planetRepository;
+	}
+	
+	@Transactional
+	public PlanetDTO getOne(int id) throws Exception {
+		
+		Optional<Planet> optional = planetRepository.findById(id);
+		
+		try {
+			
+			Planet planet = optional.get();
+			ModelMapper mapper = new ModelMapper();
+			return mapper.map(planet, PlanetDTO.class);
+			
+		}catch(Exception e) {
+			
+			throw new Exception();
+		
+		}
+		
+	}
+	
+	@Transactional
+	public List<PlanetDTO> getAll() throws Exception {
+		
+		List<Planet> planets = planetRepository.findAll();
+		List<PlanetDTO> arrayPlanetDTO = new ArrayList<>();
+		ModelMapper mapper = new ModelMapper();		
+		
+		try {
+			
+			for(Planet planet : planets) {
+				
+				PlanetDTO planetDTO = mapper.map(planet, PlanetDTO.class);
+				arrayPlanetDTO.add(planetDTO);
+				
+			}
+			
+			return arrayPlanetDTO;
+			
+		}catch(Exception e) {
+			
+			throw new Exception();
+			
+		}
+	}
+	
+	@Transactional
+	public PlanetDTO post(PlanetDTO planetDTO) throws Exception {
+		
+		Planet planet = new Planet();
+		ModelMapper mapper = new ModelMapper();
+		
+		try {
+			
+			planet = mapper.map(planetDTO, Planet.class);
+			planet = planetRepository.save(planet);
+			
+			return mapper.map(planet, PlanetDTO.class);
+			
+		}catch(Exception e) {
+			
+			throw new Exception();
+			
+		}
+		
+	}
+	
+	@Transactional
+	public PlanetDTO put(int id, PlanetDTO planetDTO) throws Exception{
+		
+		Optional<Planet> optional = planetRepository.findById(id);
+		try {
+			
+			planetDTO.setId(optional.get().getId());
+			
+			Planet planet = new Planet();
+			ModelMapper mapper = new ModelMapper();
+			planet = mapper.map(planetDTO, Planet.class);
+			planet = planetRepository.save(planet);
+			
+			return mapper.map(planet, PlanetDTO.class);
+			
+		}catch(Exception e) {
+			
+			throw new Exception();
+			
+		}
+	}
+	
+	@Transactional
+	public boolean delete(int id) throws Exception {
+		
+		try {
+			
+			if(planetRepository.existsById(id)) {
+				
+				planetRepository.deleteById(id);
+				return true;
+				
+			}else {
+				
+				throw new Exception();
+				
+			}
+		}catch(Exception e) {
+			
+			throw new Exception();
+			
+		}
+		
+	}
+	
 }
--- src/main/java/api/everis/Service/StarService.java
+++ src/main/java/api/everis/Service/StarService.java
@@ -1,5 +1,128 @@
 package api.everis.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import api.everis.DTO.StarDTO;
+import api.everis.Entity.Star;
+import api.everis.Repository.StarRepository;
+
+@Service
 public class StarService {
+	
+	private StarRepository starRepository;
+
+	public StarService(StarRepository starRepository) {
+		this.starRepository = starRepository;
+	}
+	
+	@Transactional
+	public StarDTO getOne(int id) throws Exception {
+		
+		Optional<Star> optional = starRepository.findById(id);
+		
+		try {
+			
+			Star star = optional.get();
+			ModelMapper mapper = new ModelMapper();
+			return mapper.map(star, StarDTO.class);
+			
+		}catch(Exception e) {
+			
+			throw new Exception();
+		}
+		
+	}
+	
+	@Transactional
+	public List<StarDTO> getAll() throws Exception {
+		
+		List<Star> stars = starRepository.findAll();
+		List<StarDTO> arrayStarDTO = new ArrayList<>();
+		ModelMapper mapper = new ModelMapper();
+		
+		try {
+			
+			for(Star star : stars) {
+				
+				StarDTO starDTO = mapper.map(star, StarDTO.class);
+				arrayStarDTO.add(starDTO);
+			}
+			
+			return arrayStarDTO;
+			
+		}catch (Exception e) {
+			
+			throw new Exception();
+			
+		}
+	}
+	
+	@Transactional
+	public StarDTO post(StarDTO starDTO) throws Exception {
+		
+		ModelMapper mapper = new ModelMapper();
+		
+		try {
+			
+			Star star = mapper.map(starDTO, Star.class);
+			star = starRepository.save(star);
+			return mapper.map(star, StarDTO.class);
+			
+		}catch(Exception e) {
+			
+			throw new Exception();
+		}
+	}
+	
+	@Transactional
+	public StarDTO put(int id, StarDTO starDTO) throws Exception {
+		
+		Optional<Star> optional = starRepository.findById(id);
+		Star star = new Star();
+		
+		try {
+			
+			starDTO.setId(optional.get().getId());
+			ModelMapper mapper = new ModelMapper();
+			star = mapper.map(starDTO, Star.class);
+			star = starRepository.save(star);
+			return mapper.map(star, StarDTO.class);
+			
+		}catch(Exception e) {
+			
+			throw new Exception();
+		}
+		
+	}
+	
+	@Transactional
+	public boolean delete(int id) throws Exception {
+		
+		try {
+			
+			if(starRepository.existsById(id)) {
+				
+				starRepository.deleteById(id);
+				return true;
+				
+			}else {
+				
+				throw new Exception();
+				
+			}
+			
+		}catch(Exception e) {
+			
+			throw new Exception();
+			
+		}
+	}
+	
 
 }
