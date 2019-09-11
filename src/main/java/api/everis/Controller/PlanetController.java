6fe16dc9e68d935db7ecb79d3b008aa28dc5a214
Emile <emicaron95@gmail.com>
2019-09-11 10:08:02 -0300

    Implementacion capa de Controller

--- src/main/java/api/everis/Controller/PlanetController.java
+++ src/main/java/api/everis/Controller/PlanetController.java
@@ -1,5 +1,94 @@
 package api.everis.Controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import api.everis.DTO.PlanetDTO;
+import api.everis.Service.PlanetService;
+
+@Controller
+@RestController
+@RequestMapping(path = "api/v1/planets")
 public class PlanetController {
+	
+	private PlanetService planetService;
+
+	public PlanetController(PlanetService planetService) {
+		this.planetService = planetService;
+	}
+	
+	@GetMapping("/{id}")
+	@Transactional
+	public ResponseEntity getOne(@PathVariable int id) {
+		
+		try {
+			PlanetDTO planetDTO = planetService.getOne(id);
+			return ResponseEntity.status(HttpStatus.OK).body(planetDTO);
+		}catch(Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"message\": \"Error. Please check the ID, and try again later.\"}");
+		}
+		
+	}
+	
+	@GetMapping("/")
+	@Transactional
+	public ResponseEntity getAll() {
+		
+		try {
+			List<PlanetDTO> arrayPlanetDTO = planetService.getAll();
+			return ResponseEntity.status(HttpStatus.OK).body(arrayPlanetDTO);
+		}catch(Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"message\": \"Error. Please try again later.\"}");
+		}
+		
+	}
+	
+	@PostMapping("/")
+	@Transactional
+	public ResponseEntity post(@RequestBody PlanetDTO planetDTO) {
+		
+		try {
+			return ResponseEntity.status(HttpStatus.OK).body(planetService.post(planetDTO));
+		}catch(Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"message\": \"Error. Please check the BODY request, and try again later.\"}");
+		}
+	}
+	
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity put(@PathVariable int id, @RequestBody PlanetDTO planetDTO) {
+		
+		try {
+			return ResponseEntity.status(HttpStatus.OK).body(planetService.put(id, planetDTO));
+		}catch(Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"message\": \"Error. Please check the ID or BODY request, and try again later.\"}");
+		}
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity delete(@PathVariable int id) {
+		
+		try {
+			planetService.delete(id);
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).body("");
+		}catch(Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"message\": \"Error. Please check the ID, and try again later.\"}");
+		}
+	}
 
 }
--- src/main/java/api/everis/Controller/StarController.java
+++ src/main/java/api/everis/Controller/StarController.java
@@ -1,5 +1,93 @@
 package api.everis.Controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import api.everis.DTO.StarDTO;
+import api.everis.Service.StarService;
+
+@Controller
+@RestController
+@RequestMapping(path = "api/v1/stars")
 public class StarController {
+	
+	private StarService starService;
+
+	public StarController(StarService starService) {
+		this.starService = starService;
+	}
+	
+	@GetMapping("/{id}")
+	@Transactional
+	public ResponseEntity getOne(@PathVariable int id) {
+		
+		try {
+			StarDTO starDTO = starService.getOne(id);
+			return ResponseEntity.status(HttpStatus.OK).body(starDTO);
+		}catch(Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"message\": \"Error. Please check the ID, and try again later.\"}");
+		}
+		
+	}
+	
+	@GetMapping("/")
+	@Transactional
+	public ResponseEntity getAll() {
+		
+		try {
+			List<StarDTO> arrayStarDTO = starService.getAll();
+			return ResponseEntity.status(HttpStatus.OK).body(arrayStarDTO);
+		}catch(Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"message\": \"Error. Please try again later.\"}");
+		}
+		
+	}
+	
+	@PostMapping("/")
+	@Transactional
+	public ResponseEntity post(@RequestBody StarDTO starDTO) {
+		
+		try {
+			return ResponseEntity.status(HttpStatus.OK).body(starService.post(starDTO));
+		}catch(Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"message\": \"Error. Please check the BODY request, and try again later.\"}");
+		}
+	}
+	
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity put(@PathVariable int id, @RequestBody StarDTO starDTO) {
+		
+		try {
+			return ResponseEntity.status(HttpStatus.OK).body(starService.put(id, starDTO));
+		}catch(Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"message\": \"Error. Please check the ID or BODY request, and try again later.\"}");
+		}
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity delete(@PathVariable int id) {
+		
+		try {
+			starService.delete(id);
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).body("");
+		}catch(Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"message\": \"Error. Please check the ID, and try again later.\"}");
+		}
+	}
 
 }
