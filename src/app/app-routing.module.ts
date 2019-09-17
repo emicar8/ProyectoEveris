9cd88785b48bb14a5f83209d45a0783b53057fb0
Emile <emicaron95@gmail.com>
2019-09-17 00:33:22 -0300

    Implementacion de formulario para la tabla planetas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { PlanetsComponent } from './components/planets/planets.component';
 import { HomeComponent } from './components/home/home.component';
 import { StarsComponent } from './components/stars/stars.component';
 import { StarFormComponent } from './components/star-form/star-form.component';
+import { PlanetFormComponent } from './components/planet-form/planet-form.component';
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'planets', component: PlanetsComponent},
+  { path: 'planets/:id', component: PlanetFormComponent},
   { path: 'stars', component: StarsComponent},
   { path: 'stars/:id', component: StarFormComponent},
   { path: '**', pathMatch: 'full', redirectTo: 'home'}
--- src/app/components/planet-form/planet-form.component.ts
+++ src/app/components/planet-form/planet-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Planet } from 'src/app/models/planet';
+import { PlanetService } from 'src/app/services/planet.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Star } from 'src/app/models/star';
+import { StarService } from 'src/app/services/star.service';
 
 @Component({
   selector: 'app-planet-form',
@@ -7,9 +12,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlanetFormComponent implements OnInit {
 
-  constructor() { }
+  planet : Planet = {
+    id: null,
+    name: null,
+    size: null,
+    star: null
+  };
+
+  stars : Star[] = [];
+
+  nomElegido : string = null;
+
+  constructor(private planetService : PlanetService, private starService : StarService, private activatedRoute : ActivatedRoute, private router : Router) { 
+    this.activatedRoute.params.subscribe((data)=>{
+      if(data.id != null){
+        this.planetService.getOne(data.id).subscribe((data2)=>{
+          this.planet = data2;
+        });
+      }
+    });
+    this.starService.getAll().subscribe((data)=>{
+      this.stars = data;
+    });
+  }
+
+  onClick(star : Star){
+      this.planet.star = star;
+      document.getElementById("front").innerHTML = star.name;
+  }
+
+  save(){
+    if(this.planet.id != null){
+      this.planetService.put(this.planet.id, this.planet).subscribe((data)=>{
+        location.reload();
+      });
+    }else{
+      this.planetService.post(this.planet).subscribe((data)=>{
+        this.router.navigate(['/planets/' + data.id]);
+      });
+    }
+  }
 
   ngOnInit() {
   }
+
+
 
 }
